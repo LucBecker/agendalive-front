bfadfb18f3ff820dfa974f9931630e00d9b37417
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-06-16 21:58:48 -0300

    implements form field lives

--- src/app/views/home/live-form-dialog/live-form-dialog.component.ts
+++ src/app/views/home/live-form-dialog/live-form-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { LiveService } from 'src/app/shared/service/live.service';
 
 @Component({
   selector: 'app-live-form-dialog',
@@ -14,6 +15,7 @@ export class LiveFormDialogComponent implements OnInit {
   constructor(
     public dialogRef: MatDialogRef<LiveFormDialogComponent>,
     private fb: FormBuilder,
+    private rest: LiveService
   ) { }
 
   ngOnInit(): void {
@@ -21,9 +23,15 @@ export class LiveFormDialogComponent implements OnInit {
       liveName: ['', [Validators.required]],
       channelName: ['', [Validators.required]],
       liveLink: ['', [Validators.required]],
-      liveDate: ['', [Validators.required]],
+      liveDate: ['2022-08-01T20:00:00', [Validators.required]],
       liveTime: ['', [Validators.required]]
     });
+  }
+
+  createLive(){
+    this.rest.postLives(this.liveForm.value).subscribe(result => {});
+    this.dialogRef.close();
+    this.liveForm.reset();
   }
 
   cancel(){
