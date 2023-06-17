9b0552be5a8d4417b05ebf9b1d7b61c17a1d7e03
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-06-16 21:16:10 -0300

    implements live list

--- src/app/views/home/live-list/live-list.component.ts
+++ src/app/views/home/live-list/live-list.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { LiveService } from './../../../shared/service/live.service';
+import { Component, OnInit } from '@angular/core';
+import { Live } from 'src/app/shared/model/live.model';
 
 @Component({
   selector: 'app-live-list',
   templateUrl: './live-list.component.html',
   styleUrls: ['./live-list.component.css']
 })
-export class LiveListComponent {
+export class LiveListComponent  implements OnInit {
 
+  livesNext: Live[];
+  livesPrevious: Live[];
+
+
+  constructor(
+    private liveService: LiveService,
+  ) { }
+
+  ngOnInit() {
+   this.getLives();
+  }
+
+  getLives(){
+    this.liveService.getLivesWithFlag('previous').subscribe(data => {
+      this.livesPrevious = data.content;
+      console.log(this.livesPrevious);
+    });
+
+    this.liveService.getLivesWithFlag('previous').subscribe(data => {
+      this.livesPrevious = data.content;
+      console.log(this.livesPrevious);
+    });
+  }
 }
